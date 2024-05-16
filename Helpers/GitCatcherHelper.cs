121bfc000190975d26ff0437907a2ee647399a23
boundless-bliss <qiqitomodachi@gmail.com>
2024-05-16 15:55:20 +0300

    Delete on close/merge 🥚

--- Helpers/GitCatcherHelper.cs
+++ Helpers/GitCatcherHelper.cs
@@ -42,14 +42,14 @@ namespace LLM.GitHelper.Helpers
                         var state = response.ObjectAttributes.State;
                         await _threadWatcher.AddNeededMembersToThread(threadChannel, identifiers); //if someone was mentioned add them to thread
                         await _threadWatcher.Post(threadChannel, threadedMessage); //post to thread
-                        if (state.Contains("closed") || state.Contains("merged")) await _threadWatcher.RemoveEveryone(threadChannel); //remove all on close
+                        if (state.Contains("closed") || state.Contains("merged")) await threadChannel.DeleteAsync(); //await _threadWatcher.RemoveEveryone(threadChannel); //remove all on close
                     }
                     else _debugger.Log($"Couldn't find a thread '{title}'.", new DebugOptions(this, "[THREAD NOT FOUND]"));
                 }
             }
         }
 
-        public async Task ForceCreateThreads(string title, DiscordMessageBuilder threadedMessage, 
+        public async Task ForceCreateThreads(string title, DiscordMessageBuilder threadedMessage,
             string[] identifiers, string[] loweredLookupKeys)
         {
             var channels = _broadcastDataService.GetChannels(new string[] { "all" });
